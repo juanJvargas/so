349e308ef9b6913d8e8be55493071877aeac512f
krypth <juan.jose.vargas@correounivalle.edu.co>
2017-04-05 19:27:58 -0500

    Update fileutil.c

--- fileutil.c
+++ fileutil.c
@@ -1,51 +1,61 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
+#include <stdio.h>
 #include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "fileutil.h"
 
 int bytes(char *nombre){
 	FILE *archivo;
 	int contador = 0;
         //abrimos el archivo como un archivo en formato binario de lectura
-	archivo = fopen(nombre, "rb");
+	archivo = fopen(nombre, "r");
         //mientras no se llegue al final del archivo EOF(en of file) se le sumara 1 al contador dando como resultado el tamaño del archivo
   	while(fgetc(archivo) != EOF){
   		contador++;
   	}
-        //cerramos el archivo
-  	fclose(archivo);
         //retornamos el numero de bytes
   	return contador;
 }
 
-char **listar(DIR *directorio, int *nArchivos) {
+char **listar(char *ruta, int *numeroArchivos) {
+    DIR *directorio;
+    //abrimos el directorio
+    directorio=opendir(ruta);
     //arreglo encarfado de alamacenar dentro de cada posicion un archivo que este denrto del directorio
     char **arreglo = (char **) malloc(sizeof (char *));
+    //tamaño de la ruta
+    int tamano=sizeof(ruta)+1;
+    // Comprobamos que se pudo abrir el directorio.
+    if (dir == NULL){
+        printf("No se puede abrir el directorio especificado.\n");
+	return arreglo;
+	}
     //contador que servira como ayuda para contar la cantidad de archivos que se encuentren dentro de el archivo
     int contador = 0;
     struct dirent *entrada;
-    while ((entrada = readdir(directorio)) != NULL) {
-        if ((strcmp(entrada->d_name, ".") != 0) && (strcmp(entrada->d_name, "..") != 0)) {
-            FILE *archivo;
-            //abrimos el archivo en formato de lectura
-            archivo = fopen(entrada->d_name, "r");
-            //si es un archivo entonces arreglo se posicionara en otra posicion de memoria con el objetivo de almacenar el nuevo arreglo
-            //luego se almacenara dentro de arreglo y por ultimo se aumenta contador con el objetivo de llevar la cuenta de cuantos archivos se anleido
-            if (archivo) {
-                arreglo = (char **) realloc(arreglo, (sizeof (char *) * (contador + 1)));
-                arreglo[contador] = (char *) malloc(sizeof (entrada->d_name) * sizeof (char));
-                strcpy(arreglo[contador], entrada->d_name);
-                contador++;
-            }
-            //se cierra el archivo
-            fclose(archivo);
-        }
-    }
-    //cerramos el directorio
-    closedir(directorio);
-    //almacenamos el nuemro de archivos leidos dentro de *nArchivos
-   *nArchivos = contador;
-   //retornamos el arreglo
+    //se leen todos los archivos 
+    while((entrada = readdir(dir)) != NULL){
+  		if(entrada->d_type != DT_DIR){
+			//redimencionamos el arreglo y le asignamos un nuevo tamaño en memoria para alamcenar el nombre del archivo
+  			arreglo = (char **)realloc(arreglo, (sizeof(char *) * (contador + 1)));
+            arreglo[contador] = (char *)malloc((sizeof(entrada->d_name) * sizeof(char)) + tamanoRuta);
+			//guardamos el nombre del archivo
+			strcpy(arreglo[contador], ruta);
+			strcat(arreglo[contador], "/");
+			strcat(arreglo[contador], entrada->d_name);
+			contador++;		
+    	}
+	}	
+    
+    // Cerramos el directorio.
+	closedir (dir);
+    
+	// Guardamos la cantidad de archivos encontrados.
+    *numeroArchivos = contador;
+    
     return arreglo;
 }
